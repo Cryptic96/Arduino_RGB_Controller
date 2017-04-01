5cd88d0a0a015102b45082b378ef1c508af11459
Unknown <theunschut@gmail.com>
2017-04-01 12:37:37 +0200

    Arduino Sketch

---
+++ Arduino Sketches/Console_Input_Test/Console_Input_Test.ino
@@ -0,0 +1,28 @@
+const int ledPin =  LED_BUILTIN;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  
+  if(Serial.available() > 0) {
+    String str = Serial.readString();
+    str.replace("\n", "");
+    str.toUpperCase();
+    
+    if(str.equals("HIGH")) {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("Led ON");
+      
+    } else if(str == "LOW") {
+      digitalWrite(ledPin, LOW);
+      Serial.println("Led OFF");
+      
+    } else {
+      Serial.println("Unknown Command");
+      //Serial.println("You typed: '" + str + "'");
+    }
+  }
+}
